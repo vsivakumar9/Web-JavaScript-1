01975c71cc939f6c80cbfae58ee0cddfdcc69b8b
ShivaNJ <32874023+vsivakumar9@users.noreply.github.com>
2018-08-22 14:55:08 -0400

    JS add table data  - baseline commit 8-22b

--- static/js/app.js
+++ static/js/app.js
@@ -1,4 +1,13 @@
 // from data.js
 var tableData = data;
 
-// YOUR CODE HERE!
+// Select all tbody from using d3 select and add  table rows dynamically from tableData.
+d3.select("tbody")
+  .selectAll("tr")
+  .data(tableData)
+  .enter()
+  .append("tr")
+  .html(function(d) {
+    return `<td>${d.datetime}</td> <td>${d.city}</td> <td>${d.state}</td> <td>${d.country}</td>
+            <td>${d.shape}</td> <td>${d.durationMinutes}</td> <td>${d.comments}</td>     `;
+  });
