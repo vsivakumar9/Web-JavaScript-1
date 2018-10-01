731dd24c3d24f3702f294a72356df86ae7ed3935
ShivaNJ <32874023+vsivakumar9@users.noreply.github.com>
2018-10-01 15:38:13 -0400

    version 2.0 with date and state filters 10-1a

--- static/js/app.js
+++ static/js/app.js
@@ -1,15 +1,25 @@
 // javaScript to load table data into index.html. If a date is provided as a filter, retrieve
 // the date and reload with filtered data only into the DOM.
 // data  is defined in  data.js
+// Author. Siva V. 
 var tableData = data;
 
 //Getting a reference to the button on the page with the id propertyfilter-btn 
-var button = d3.select("#filter-btn");
-// Getting a reference to the input element on the page with the id property set to 'input-field'
+var filtButton = d3.select("#filter-btn");
+// Getting a reference to the input element on the page with the id property set to 'datetime'
 var inputDate = d3.select("#datetime");
-//Getting a reference to state
+
+//Create a var for the d3select of tbody.
+var inputTbody = d3.select("tbody");
+
+//Getting a reference to state and other fields. 
 var inputState = d3.select("#state");
-var defDate = '1/11/2010'
+var inputCountry = d3.select("#country")
+var inputShape   = d3.select("#shape")
+
+var defDate = '1/1/2010';
+var loadFiltData = "n";
+var dateInput ;
 
 // Select all tbody from using d3 select and add  table rows dynamically from tableData.
 function loadTableData(dataArray) {
@@ -22,86 +32,66 @@ d3.select("tbody")
     return `<td>${d.datetime}</td> <td>${d.city}</td> <td>${d.state}</td> <td>${d.country}</td>
             <td>${d.shape}</td> <td>${d.durationMinutes}</td> <td>${d.comments}</td>     `;
   });
-
 }
 
-// // This function is triggered when the button filter-data is clicked.
-// function handleClick() {
-//   console.log("A button was clicked!");
-//   // We can use d3 to see the object that dispatched the event
-//   console.log(d3.event.target);
-// }
-
-// // We can use the `on` function in d3 to attach an event to the handler function.
-// function getInput () {
-//   button.on("click", handleClick);
-// }
-var iDate  = defDate
-function getInput() {
-  console.log("in getInput function");
-  //Input fields can trigger a change event when new text is entered.
-  inputDate.on("change", function() {
-    d3.event.preventDefault();
-    var newDate = d3.event.target.value
-    if (newDate) {
-      console.log(newDate)
-      var iDate  = newDate
-      console.log("New Date: " + iDate)
-      } 
-    else {
-      iDate = defDate
-      console.log("default date :" + iDate )
-      }
-  
-    return(iDate)
-  });
-
-}
-
-inputState.on("change",function(){
-  d3.event.preventDefault();
-  var newState = d3.event.target.value
-  console.log("New State: " +  newState)
-});
-
-function filterData(date) {
-
-  console.log("In filterData" + date)
+//Filter the data based on input parameters like data and only return values for which
+// //criteria is satisfied. 
+// function filterData(date) {
+//   console.log("In filterData" + date)
+//   if (date)
+//     var filtData = tableData.filter(fData => fData.datetime == date);
+//     //loadFiltData = 'y';
+//     console.log("loadFiltData " + FiltData)
+//     console.log(filtData);
+//     return(filtData)
       
-}
+//}
 
 //Main logic.
-// Load all the table data of UFO sightings and render in html template. 
-loadTableData(tableData);
+// Load all the table data of UFO sightings and render in html template.
 
-// You can also define the click handler inline
-button.on("click", function() {
-  // Prevent the form from refreshing the page
-  d3.event.preventDefault();
-  console.log("Hi, filter button was clicked!");
-  console.log(d3.event.target);
-  //Filter the UFO sightings data based on the date and reload table in DOM.
-  filterData(iDate) ;
+if (loadFiltData == 'y') {
+  console.log("in filt data y")
+  finalData = filtData;
+  }
+else {
+  console.log("in filt data n")
+  finalData = tableData;
+  }
+console.log("First time main logic")
+console.log(finalData)
+inputTbody.html("");
+loadTableData(finalData)
+
+// User clicks the button to filter data
+filtButton.on("click", function() {
+
+ // Prevent the whole page from refreshing.
+ d3.event.preventDefault();
+
+ // Get the value property of the input element
+ var dateValue = inputDate.property("value");
+ var stateValue = inputState.property("value");
+
+ console.log(" in filtbutton.on code ")
+ console.log(dateValue);
+ console.log(stateValue);
+ console.log(" after getting date/state value")
+ var filtData = tableData.filter(fData => (fData.datetime == dateValue) && fData.state == stateValue) ;
+ console.log(filtData) 
+ loadFiltData = "y";
+ if (loadFiltData == 'y') {
+  console.log("in filt data y")
+  finalData = filtData;
+  }
+else {
+  console.log("in filt data n")
+  finalData = tableData;
+  }
+console.log(finalData)
+//Clear all previuos data from UFO table
+inputTbody.html("");
+loadTableData(finalData)
 });
-
-//On a submit, get date from the html form.
-dateInput = getInput();
-
-// inputDate.on("change", function() {
-//   d3.event.preventDefault();
-//   var newDate = d3.event.target.value
-//   if (newDate) {
-//     console.log(newDate)
-//     var iDate  = newDate
-//     console.log("New Date: " + iDate)
-//     } 
-//   else {
-//     iDate = '1/11/2011'
-//     console.log("default date :" + iDate )
-//     }
-
-//  //return(newDate)
-// });
-
 
 
